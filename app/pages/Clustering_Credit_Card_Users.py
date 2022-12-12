91fc18932cdf68e7b89d5a71f4f0916ee2f8230e
alimtegar <alimtegar4@gmail.com>
2022-12-12 10:09:28 +0700

    Update final project 4

---
+++ app/pages/Clustering_Credit_Card_Users.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.title('Clustering Credit Card Users')
+
+with open('../exported-files/models/cc_users_clusterer.pkl', 'rb') as file:
+  model = pickle.load(file)
+    
+@st.cache
+def predict(balance, balance_frequency, purchases, one_off_purchases, installment_purchases, cash_advance, purchases_frequency, one_off_purchases_frequency, installment_purchases_frequency, cash_advance_frequency, cash_advance_trx, purchases_trx, credit_limit, minimum_payments, full_payment, tenure):
+  return model.predict(pd.DataFrame([[balance, balance_frequency/100, purchases, one_off_purchases, installment_purchases, cash_advance, purchases_frequency/100, one_off_purchases_frequency/100, installment_purchases_frequency/100, cash_advance_frequency/100, cash_advance_trx, purchases_trx, credit_limit, minimum_payments, full_payment/100, tenure]]))
+
+balance = st.number_input('Balance:', min_value=0.0)
+balance_frequency = st.slider('Balance Frequncy:', 0, 100, format='%f%%')
+purchases = st.number_input('Purchases:', min_value=0.0)
+one_off_purchases = st.number_input('One Off Purchases:', min_value=0.0)
+installment_purchases = st.number_input('Installment Purchases:', min_value=0.0)
+cash_advance = st.number_input('Cash Advance:', min_value=0.0)
+purchases_frequency = st.slider('Purchases Frequncy:', 0, 100, format='%f%%')
+one_off_purchases_frequency = st.slider('One Off Purchases Frequncy:', 0, 100, format='%f%%')
+installment_purchases_frequency = st.slider('Installment Purchases Frequncy:', 0, 100, format='%f%%')
+cash_advance_frequency = st.slider('Cash Advance Frequncy:', 0, 100, format='%f%%')
+cash_advance_trx = st.number_input('Cash Advance Transactions:', min_value=0)
+purchases_trx = st.number_input('Purchases Transactions:', min_value=0)
+credit_limit = st.number_input('Credit Limit:', min_value=0.0)
+minimum_payments = st.number_input('Minimum Payments:', min_value=0.0)
+full_payment = st.slider('Percent of Full Payment:', 0, 100, format='%f%%')
+tenure = st.number_input('Tenure:', min_value=0)
+
+if st.button('Predict'):
+  prediction = predict(balance, balance_frequency, purchases, one_off_purchases, installment_purchases, cash_advance, purchases_frequency, one_off_purchases_frequency, installment_purchases_frequency, cash_advance_frequency, cash_advance_trx, purchases_trx, credit_limit, minimum_payments, full_payment, tenure)
+  
+  print(prediction)
+  # if prediction[0]:
+  #   st.error('You are at high risk of heart failure.')
+  # else:
+  #   st.success('You are at low risk of heart failure.')
